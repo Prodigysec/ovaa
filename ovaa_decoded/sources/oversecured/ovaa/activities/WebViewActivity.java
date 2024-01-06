a6da7eb0d2c6f94677d66f15782d3426e938bf3b
Ian Raburu <williamraburu@gmail.com>
2024-01-06 23:51:54 +0300

    Add decoded

---
+++ ovaa_decoded/sources/oversecured/ovaa/activities/WebViewActivity.java
@@ -0,0 +1,27 @@
+package oversecured.ovaa.activities;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import oversecured.ovaa.R;
+/* loaded from: classes.dex */
+public class WebViewActivity extends AppCompatActivity {
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // androidx.appcompat.app.AppCompatActivity, androidx.fragment.app.FragmentActivity, androidx.activity.ComponentActivity, androidx.core.app.ComponentActivity, android.app.Activity
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        setupWebView(webView);
+        webView.loadUrl(getIntent().getStringExtra("url"));
+    }
+
+    private void setupWebView(WebView webView) {
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+    }
+}
